3dd0c292c27497a8668e6fb553e88b1273468cb7
ldci <francois.jouen@ephe.sorbonne.fr>
2014-03-18 14:56:16 +0100

    update
    samples updated

--- samples/rg_edges.r
+++ samples/rg_edges.r
@@ -20,14 +20,14 @@ isfile: false
 ;when moving slider
 onTrackbar: does [
     edge_thresh: sl1/data * max_thresh					;update threshold with slider
-    cvSmooth gray edge CV_BLUR 3 3 0 0             	;filter
-    cvNot gray edge									;element bit-wise inversion of array elements
-    cvCanny gray edge edge_thresh edge_thresh * 3 3 	;Run the edge detector on grayscale
-    cvZero cedge										;color edges window to black
-    cvCopy image cedge edge							;copy edge points
+    cvSmooth &gray &edge CV_BLUR 3 3 0 0             	    ;filter
+    cvNot &gray &edge										;element bit-wise inversion of array elements
+    cvCanny &gray &edge edge_thresh edge_thresh * 3 3 	;Run the edge detector on grayscale
+    cvZero &cedge										;color edges window to black
+    cvCopy &image &cedge &edge								;copy edge points
     ; copy to rebol image
-    either edge_thresh = 0 [cvtoRebol image rimage1] [cvtoRebol cedge rimage1]
-    
+    either edge_thresh = 0 [cvtoRebol image rimage1]
+    [cvtoRebol cedge rimage1] 
     oct/text: to-string round/to edge_thresh 0.01
     show oct
     recycle
@@ -42,30 +42,28 @@ loadImage: does [
 		if error? try [
 		    ; load image
 			image: cvLoadImage filename CV_LOAD_IMAGE_COLOR; CV_LOAD_IMAGE_UNCHANGED 
+			&image: as-pointer! image
 			;Create the color output image
 			cedge: cvCreateImage image/width image/height IPL_DEPTH_8U 3
+			&cedge: as-pointer! cedge
 			;// Convert to grayscale
-    		gray: cvCreateImage image/width image/height IPL_DEPTH_8U 1
-    		edge: cvCreateImage image/width image/height IPL_DEPTH_8U 1
-    		cvCvtColor image gray CV_BGR2GRAY
+    		&gray: as-pointer! cvCreateImage image/width image/height IPL_DEPTH_8U 1
+    		&edge: as-pointer! cvCreateImage image/width image/height IPL_DEPTH_8U 1
+    		cvCvtColor &image &gray CV_BGR2GRAY
     		;show rebol image
-    		cvtoRebol image rimage1 
+			cvtoRebol image rimage1 
 			isFile: true
 			sl1/data: 0
 			show sl1
 		]
-		[Alert "Not an image" ]
+		[Alert "Problem in reading image" ]
 	]
 ]
-
-
-
-
 
 mainWin: [
 	at 1x0 button 25 "Load Image" [loadImage] 
 	at 112x1 button 25 "Quit" [if isFile [cvReleaseImage image cvReleaseImage cedge
-					cvReleaseImage gray cvReleaseImage edge] 
+					cvReleaseImage &gray cvReleaseImage &edge] 
 					quit]
 	at 1x8 field 20 "Threshold" options [info] sl1: slider 60x5 options [arrows][if isFile [onTrackbar]] 
 	oct: field 12 "0.0" options [info] font [align: 'center]
